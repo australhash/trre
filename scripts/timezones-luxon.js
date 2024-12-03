2a4d17da972363435e28feb43cac05dbfe6fc9a1
australhash <australhash@gmail.com>
2024-12-03 16:37:32 +0100

    Update timezones-luxon.js

--- scripts/timezones-luxon.js
+++ scripts/timezones-luxon.js
@@ -2,18 +2,16 @@ const locations = document.querySelectorAll(".footer--hour")
 
 
 
-const updateTimes = function() {
+const updateTimes = () => {
 	locations.forEach(location => {
 		const zone = location.querySelector(".time--footer")
 		const timezone = location.getAttribute("data-timezone")
 		
 		const now = luxon.DateTime.now().setZone(timezone)
-		zone.innerHTML = now.toFormat("HH:mm:ss")
+		zone.innerHTML = now.toFormat("tt")
 	})
 }
 
 updateTimes()
 
-setInterval(function () {
-	updateTimes()
-}, 1000)
+setInterval( () => { updateTimes() }, 1000)
